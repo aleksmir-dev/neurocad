9e1b60e3a911a02f15e5bf6e720915ad6041cbea
aleksmir <aleksmir@list.ru>
2026-09-16 11:05:12 +0000

    .

--- app/static/core/base/footer/footer.js
+++
@@ -1,8 +0,0 @@
-// app/core/base/footer/footer.js
-
-class CoreBaseFooter {
-    constructor(baseInstance) {
-        this.base = baseInstance;
-        this.container = document.querySelector('.core-base-footer');
-    }
-}
